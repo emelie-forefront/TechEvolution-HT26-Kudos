76dcbc631bfe922a94d76abdc4882e0d7d8d19eb
Emelie Boss <emelie.boss@forefront.se>
2026-09-08 11:07:51 +0200

    feat(feed): add pagination

--- src/features/feed/KudosFeed.tsx
+++ src/features/feed/KudosFeed.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { Kudos } from '../../domain/kudos'
+import { FeedPagination } from './FeedPagination'
 import { FeedSortControl } from './FeedSortControl'
 import { KudosCard } from './KudosCard'
 import { sortKudos, type FeedSort } from './feedSorting'
@@ -9,16 +10,31 @@ type KudosFeedProps = {
   onCreateKudos: () => void
 }
 
+const PAGE_SIZE = 10
+
 export function KudosFeed({ kudos }: KudosFeedProps) {
   const [sort, setSort] = useState<FeedSort>('date-newest')
+  const [page, setPage] = useState(1)
   const sortedKudos = sortKudos(kudos, sort)
+  const pageCount = Math.max(1, Math.ceil(sortedKudos.length / PAGE_SIZE))
+  const pageKudos = sortedKudos.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
+
+  useEffect(() => {
+    setPage((currentPage) => Math.min(currentPage, pageCount))
+  }, [pageCount])
+
+  function handleSortChange(nextSort: FeedSort) {
+    setSort(nextSort)
+    setPage(1)
+  }
 
   return (
     <>
-      <FeedSortControl value={sort} onChange={setSort} />
+      <FeedSortControl value={sort} onChange={handleSortChange} />
       <div className="kudos-feed">
-        {sortedKudos.map((entry) => <KudosCard key={entry.id} kudos={entry} />)}
+        {pageKudos.map((entry) => <KudosCard key={entry.id} kudos={entry} />)}
       </div>
+      <FeedPagination page={page} pageCount={pageCount} onPageChange={setPage} />
     </>
   )
 }
